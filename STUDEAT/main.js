bd0c36802210b8e183e7c3db4d3882b0ffa77c3e
TimothyODONOHUE <114920263+TimothyODONOHUE@users.noreply.github.com>
2023-02-21 11:43:57 +0100

    Add files via upload

--- STUDEAT/main.js
+++ STUDEAT/main.js
@@ -1,8 +1,64 @@
-const boutons = document.querySelectorAll(".clickable")
+const tailleM = document.querySelector(".tailleM")
+const tailleL = document.querySelector(".tailleL")
+const tailleXL = document.querySelector(".tailleXL")
 
 
-boutons.forEach((bouton)=>{
-    bouton.addEventListener("click", ()=>{
-        bouton.classList.toggle("jaune")
-})})
+tailleM.addEventListener("click", ()=>{
+
+    if(!tailleM.classList.contains("jaune")){
+            tailleL.classList.remove("jaune")
+            tailleXL.classList.remove("jaune")
+            tailleM.classList.add("jaune")
+    }
+})
+
+tailleL.addEventListener("click", ()=>{
+
+    if(!tailleL.classList.contains("jaune")){
+            tailleM.classList.remove("jaune")
+            tailleXL.classList.remove("jaune")
+            tailleL.classList.add("jaune")
+    }
+})
+
+tailleXL.addEventListener("click", ()=>{
+
+    if(!tailleXL.classList.contains("jaune")){
+            tailleL.classList.remove("jaune")
+            tailleM.classList.remove("jaune")
+            tailleXL.classList.add("jaune")
+    }
+})
+
+const viandesteak = document.querySelector(".viandesteak")
+const viandenuggets = document.querySelector(".viandenuggets")
+const viandekebab = document.querySelector(".viandekebab")
+
+
+viandesteak.addEventListener("click", ()=>{
+
+    if(!viandesteak.classList.contains("jaune")){
+            viandenuggets.classList.remove("jaune")
+            viandekebab.classList.remove("jaune")
+            viandesteak.classList.add("jaune")
+    }
+})
+
+viandenuggets.addEventListener("click", ()=>{
+
+    if(!viandenuggets.classList.contains("jaune")){
+            viandesteak.classList.remove("jaune")
+            viandekebab.classList.remove("jaune")
+            viandenuggets.classList.add("jaune")
+    }
+})
+
+viandekebab.addEventListener("click", ()=>{
+
+    if(!viandekebab.classList.contains("jaune")){
+            viandesteak.classList.remove("jaune")
+            viandenuggets.classList.remove("jaune")
+            viandekebab.classList.add("jaune")
+    }
+})
 
